ef898be479c52f59bd7c7b73173fc1fabbd2aff4
sushilsree98 <sushil.sree98@gmail.com>
2020-03-08 14:43:17 +0530

    files added

---
+++ index.js
@@ -0,0 +1,18 @@
+exports.assocPath =  function(arr,num,obj){
+       obj[arr[arr.length-1]]=num;
+       obj[arr[0]]=undefined;
+       obj=JSON.parse(JSON.stringify(obj))
+       ar=[...arr];
+       ar=ar.reverse();
+       ar=ar.slice(arr.length-2);
+       ar.map((ele)=>{
+           let x={};
+           x[ele]=obj;
+           obj=x;
+       })
+       return obj
+   }
+
+ exports.merge=function(obj1,obj2){
+      return Object.assign(obj1,obj2);
+ }
