58b57184dddbcd424627a33e39985f58c36600ae
sushilsree98 <sushil.sree98@gmail.com>
2020-03-11 09:16:40 +0530

    few more functions added

--- index.js
+++ index.js
@@ -16,3 +16,26 @@ exports.assocPath =  function(arr,num,obj){
  exports.merge=function(obj1,obj2){
       return {...obj1,...obj2};
  }
+
+ exports.append=function(str1,arr){
+   str1=JSON.stringify(str1);
+   arr.push(JSON.parse(str1));
+   return arr;
+ }
+
+  exports.applyTo=function(val){
+      let that=val;
+      return function(fn){
+       return fn(that);
+     }
+
+    }
+  exports.identity=function(val){
+       return val;
+     }
+
+  exports.add=function(val){
+       return function(sum){
+         return sum+val;
+       }
+     }
